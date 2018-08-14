bdff44f93707dc0c39aec5b81d3a123f36a32891
Sergii Pytel <sergiyonepitel@gmail.com>
2018-08-14 22:38:11 +0300

    Remove redundant utils folder

--- scrapper/locators.py
+++ scrapper/locators.py
@@ -1,7 +1,7 @@
 class By(object):
-    def __init__(self, tag_name=None, class_=None, id=None, parent=None):
+    def __init__(self, tag_name=None, class_=None, id=None, container=None):
         self.tag_name = tag_name
-        self.parent = parent
+        self.container = container
         self.__class = class_
         self.__id = id
 
@@ -17,33 +17,33 @@ class By(object):
 
 PRODUCT_NAME_LOCATORS = [
     # https://bt.rozetka.com.ua
-    By(tag_name='h1', class_='detail-title', parent=By(tag_name='div', class_='detail-title-wrap')),
+    By(tag_name='h1', class_='detail-title', container=By(tag_name='div', class_='detail-title-wrap')),
     # https://ikea-club.com.ua
-    By(tag_name='h1', parent=By(tag_name='div', class_='product-name')),
+    By(tag_name='h1', container=By(tag_name='div', class_='product-name')),
     # https://e27.com.ua
-    By(tag_name='h1', parent=By(tag_name='div', class_='product-name')),
+    By(tag_name='h1', container=By(tag_name='div', class_='product-name')),
     # https://jysk.ua
-    By(tag_name='h1', parent=By(tag_name='div', class_='product-name-sku'))
+    By(tag_name='h1', container=By(tag_name='div', class_='product-name-sku'))
 ]
 
 PRODUCT_IMAGE_LOCATORS = [
     # https://bt.rozetka.com.ua
-    By(tag_name='img', id='base_image', parent=By(tag_name='div', class_='responsive-img')),
+    By(tag_name='img', id='base_image', container=By(tag_name='div', class_='responsive-img')),
     # https://ikea-club.com.ua
-    By(tag_name='img', id='cloudZoomImage', parent=By(tag_name='div', id='sevenspikes-cloud-zoom')),
+    By(tag_name='img', id='cloudZoomImage', container=By(tag_name='div', id='sevenspikes-cloud-zoom')),
     # https://e27.com.ua
-    By(tag_name='img', id='image-main', parent=By(tag_name='div', class_='img-box')),
+    By(tag_name='img', id='image-main', container=By(tag_name='div', class_='img-box')),
     # https://jysk.ua
-    By(tag_name='img', class_='image', parent=By(tag_name='div', id='product-image-carousel'))
+    By(tag_name='img', class_='image', container=By(tag_name='div', id='product-image-carousel'))
 ]
 
 PRODUCT_PRICE_LOCATORS = [
     # https://bt.rozetka.com.ua
-    By(tag_name='meta', parent=By(tag_name='div', class_='detail-price-uah')),
+    By(tag_name='meta', container=By(tag_name='div', class_='detail-price-uah')),
     # https://ikea-club.com.ua
-    By(tag_name='span', parent=By(tag_name='div', class_='product-price')),
+    By(tag_name='span', container=By(tag_name='div', class_='product-price')),
     # https://e27.com.ua
-    By(tag_name='span', class_='price', parent=By(tag_name='div', class_='product-type-data')),
+    By(tag_name='span', class_='price', container=By(tag_name='div', class_='product-type-data')),
     # https://jysk.ua
-    By(tag_name='span', class_='product-price', parent=By(tag_name='div', class_='product-sumup'))
+    By(tag_name='span', class_='product-price', container=By(tag_name='div', class_='product-sumup'))
 ]
