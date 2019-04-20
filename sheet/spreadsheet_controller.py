169da77dd062b6f89f0a33b9a8ca07cffe7f6eee
Sergii Pytel <sergiyonepitel@gmail.com>
2019-04-20 18:46:43 +0300

    Refactor spreadsheet_controller

--- sheet/spreadsheet_controller.py
+++ sheet/spreadsheet_controller.py
@@ -3,6 +3,8 @@ import re
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+from scrapper.product_scrapper import ProductDetailsScrapper
+
 
 class SpreadSheetController(object):
     SCOPE = ['https://spreadsheets.google.com/feeds',
@@ -12,21 +14,40 @@ class SpreadSheetController(object):
     COLUMN_PRODUCT_NAME_INDEX = 3
     COLUMN_PRODUCT_IMAGE_INDEX = 2
     COLUMN_PRODUCT_PRICE_INDEX = 6
+    scrapper = ProductDetailsScrapper()
 
     def __init__(self, worksheet_index, sheet_name):
         self.__credentials = ServiceAccountCredentials.from_json_keyfile_name(self.CLIENT_SECRET_PATH, self.SCOPE)
         self.__client = gspread.authorize(self.__credentials)
         self.working_sheet = self.__client.open(sheet_name).get_worksheet(worksheet_index)
 
-    def get_all_working_cells(self):
+    def __get_all_working_cells(self):
         search_criteria = re.compile(r'(http|https)')
         return self.working_sheet.findall(search_criteria)
 
-    def update_product_name(self, cell, name):
+    def __update_product_name(self, cell, name):
         self.working_sheet.update_cell(cell.row, self.COLUMN_PRODUCT_NAME_INDEX, name)
 
-    def update_product_image(self, cell, image_url):
+    def __update_product_image(self, cell, image_url):
         self.working_sheet.update_cell(cell.row, self.COLUMN_PRODUCT_IMAGE_INDEX, '=image("{url}")'.format(url=image_url))
 
-    def update_product_price(self, cell, price):
+    def __update_product_price(self, cell, price):
         self.working_sheet.update_cell(cell.row, self.COLUMN_PRODUCT_PRICE_INDEX, price)
+
+    def clear_spreadsheet(self):
+        products_url_cells = self.__get_all_working_cells()
+        for cell in products_url_cells:
+            self.__update_product_name(cell, '')
+            self.__update_product_image(cell, '')
+            self.__update_product_price(cell, 0)
+
+    def update_spreadsheet(self):
+        products_url_cells = self.__get_all_working_cells()
+        for cell in products_url_cells:
+            product = self.scrapper.get_product_details(cell.value)
+            if product.name:
+                self.__update_product_name(cell, product.name)
+            if product.price:
+                self.__update_product_price(cell, product.price)
+            if product.image_url:
+                self.__update_product_image(cell, product.image_url)
--- spec_update.py
+++ spec_update.py
@@ -1,32 +1,7 @@
-#!/usr/bin/python
 from sheet.spreadsheet_controller import SpreadSheetController
-from scrapper.product_scrapper import ProductDetailsScrapper
 
 sheet_controller = SpreadSheetController(worksheet_index=0, sheet_name='ptzn_spec_test')
-scrapper = ProductDetailsScrapper()
-
-
-# Use only for debug purposes
-def clear_spreadsheet():
-    products_url_cells = sheet_controller.get_all_working_cells()
-    for cell in products_url_cells:
-        sheet_controller.update_product_name(cell, '')
-        sheet_controller.update_product_image(cell, '')
-        sheet_controller.update_product_price(cell, 0)
-
-
-def update_spreadsheet():
-    products_url_cells = sheet_controller.get_all_working_cells()
-    for cell in products_url_cells:
-        product = scrapper.get_product_details(cell.value)
-        if product.name:
-            sheet_controller.update_product_name(cell, product.name)
-        if product.price:
-            sheet_controller.update_product_price(cell, product.price)
-        if product.image_url:
-            sheet_controller.update_product_image(cell, product.image_url)
-
 
 if __name__ == '__main__':
-    update_spreadsheet()
-    # clear_spreadsheet()
+    sheet_controller.update_spreadsheet()
+    # sheet_controller.clear_spreadsheet()
